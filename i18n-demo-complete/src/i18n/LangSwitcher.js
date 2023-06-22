aec9b11c84e52d1289404ea91a59366d6f2eee49
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-06-22 14:49:07 +0100

    design: update language selector styles

--- i18n-demo-complete/src/i18n/LangSwitcher.js
+++ i18n-demo-complete/src/i18n/LangSwitcher.js
@@ -3,15 +3,15 @@ import { locales } from "./i18n-config";
 import { LocaleContext } from "./LocaleContext";
 import { setStoredLocale } from "./stored-locale";
 
-export default function LangSwitcher({ onLangChanged }) {
+export default function LangSwitcher() {
   const { locale, setLocale } = useContext(LocaleContext);
 
   return (
-    <div className="flex">
+    <div className="flex relative items-center bg-amber-900 text-amber-200 rounded-md py-[2px] ltr:pl-1 rtl:pr-1 after:content-[''] after:w-3 after:h-3 after:bg-amber-200 after:clip-arrow after:block after:absolute after:top-[10px] ltr:after:right-2 rtl:after:left-2">
       <img
         src="/noun-language-4114572.svg"
         alt="Langage switcher icon"
-        className="w-4 h-4 mt-1 ltr:mr-1 rtl:ml-1"
+        className="w-4 h-4 ltr:mr-1 rtl:ml-1"
       />
 
       <select
@@ -20,7 +20,7 @@ export default function LangSwitcher({ onLangChanged }) {
           setLocale(e.target.value);
           setStoredLocale(e.target.value);
         }}
-        className="bg-transparent"
+        className="bg-transparent appearance-none ltr:pr-5 rtl:pl-5"
       >
         {Object.keys(locales).map((loc) => (
           <option value={loc} key={loc}>
