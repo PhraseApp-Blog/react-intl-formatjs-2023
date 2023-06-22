4ce3e19da1744bd6599fedae8602112482b237db
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-06-22 13:14:35 +0100

    feat: dynamic message loading

--- i18n-demo-complete/src/i18n/I18n.js
+++ i18n-demo-complete/src/i18n/I18n.js
@@ -1,17 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IntlProvider } from "react-intl";
-import { defaultLocale, locales } from "./i18n-config";
+import { defaultLocale } from "./i18n-config";
 import { LocaleContext } from "./LocaleContext";
 
 export default function I18n(props) {
   const [locale, setLocale] = useState(defaultLocale);
+  const [messages, setMessages] = useState(null);
 
-  return (
+  useEffect(() => {
+    setMessages(null);
+
+    import(`../lang/${locale}.json`)
+      .then((messages_) => setMessages(messages_))
+      .catch((err) =>
+        console.error(`Error loading messages for locale ${locale}: `, err)
+      );
+  }, [locale]);
+
+  return !messages ? (
+    <p>Loading...</p>
+  ) : (
     <LocaleContext.Provider value={{ locale, setLocale }}>
       <IntlProvider
         locale={locale}
         defaultLocale={defaultLocale}
-        messages={locales[locale].messages}
+        messages={messages}
       >
         {props.children}
       </IntlProvider>
--- i18n-demo-complete/src/i18n/i18n-config.js
+++ i18n-demo-complete/src/i18n/i18n-config.js
@@ -1,17 +1,12 @@
-import enMessages from "../lang/en-US.json";
-import arMessages from "../lang/ar-EG.json";
-
 export const defaultLocale = "en-US";
 
 export const locales = {
   "en-US": {
     name: "English",
-    messages: enMessages,
     dir: "ltr",
   },
   "ar-EG": {
     name: "Arabic (العربية)",
-    messages: arMessages,
     dir: "rtl",
   },
 };
