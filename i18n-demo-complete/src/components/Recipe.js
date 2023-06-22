2540e989d7d7f05308b6c0671401492d30814a51
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-06-22 11:05:16 +0100

    feat: interpolation

--- i18n-demo-complete/src/components/Recipe.js
+++ i18n-demo-complete/src/components/Recipe.js
@@ -1,22 +1,33 @@
+import { FormattedMessage, useIntl } from "react-intl";
 import Nutrition from "./Nutrition";
 
-export default function Recipe(props) {
+export default function Recipe() {
+  const intl = useIntl();
+
   return (
     <main className="bg-stone-950 w-full rounded-lg shadow-lg overflow-hidden mt-4 px-4 pb-4">
       <h2 className="text-amber-200 text-xs font-thin mt-2 mb-1">
-        Today's recipe
+        <FormattedMessage id="recipe.title_label" />
       </h2>
       <h3 className="ltr:bg-gradient-to-r rtl:bg-gradient-to-l from-amber-300 to-amber-500 text-stone-950 text-lg font-bold mb-2 px-2 py-1 rounded-sm">
-        Delightful Vegan Bean Burger
+        <FormattedMessage id="recipe.title" />
       </h3>
       <div className="flex">
         <div className="w-[200px] ltr:mr-4 rtl:ml-4">
-          <img src="/vegan_burger.jpg" alt="Vegan burger on a wooden plate" />
+          <img
+            src="/vegan_burger.jpg"
+            alt={intl.$t({ id: "recipe.img_alt" })}
+          />
         </div>
 
         <div className="grow">
           <div className="flex justify-between items-baseline mb-1">
-            <p className="text-amber-300">by Rabia Mousa</p>
+            <p className="text-amber-300">
+              <FormattedMessage
+                id="recipe.author"
+                values={{ author: "Rabia Mousa" }}
+              />
+            </p>
             <p className="text-amber-300 text-xs font-thin">2023/6/20</p>
           </div>
 
