637a5aae0af3995379c44061a2be97c6f31079fc
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-06-23 18:43:01 +0100

    refactor: destructuring formatMessage in useDocL10n hook

--- i18n-demo-complete/src/i18n/useDocL10n.js
+++ i18n-demo-complete/src/i18n/useDocL10n.js
@@ -3,10 +3,10 @@ import { useIntl } from "react-intl";
 import { locales } from "./i18n-config";
 
 export function useDocL10n() {
-  const intl = useIntl();
+  const { locale, formatMessage } = useIntl();
 
   useEffect(() => {
-    document.dir = locales[intl.locale].dir;
-    document.title = intl.formatMessage({ id: "app.title" });
-  }, [intl, intl.locale]);
+    document.dir = locales[locale].dir;
+    document.title = formatMessage({ id: "app.title" });
+  }, [locale, formatMessage]);
 }
