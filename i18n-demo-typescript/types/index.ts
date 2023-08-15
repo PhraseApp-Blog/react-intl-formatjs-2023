616b5b303a839a16d3a78ce17b8e977f7c3e38ea
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-08-15 18:32:30 +0100

    feat: add type-safe message IDs

--- i18n-demo-typescript/types/index.ts
+++ i18n-demo-typescript/types/index.ts
@@ -1,4 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface Navigator {
-  userLanguage?: string;
+import messages from "../src/lang/en-US.json";
+
+declare global {
+  interface Navigator {
+    userLanguage?: string;
+  }
+
+  namespace FormatjsIntl {
+    interface Message {
+      ids: keyof typeof messages;
+    }
+  }
 }
