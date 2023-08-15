07f337c68a8d03f0aacc819f62803f060f5bfee0
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-08-15 13:07:49 +0100

    build: add Phrase config

--- i18n-demo-extraction/scripts/compile-i18n.mjs
+++ i18n-demo-extraction/scripts/compile-i18n.mjs
@@ -2,7 +2,7 @@ import { locales } from "../src/i18n/i18n-config.mjs";
 import { exec } from "child_process";
 
 for (const locale of Object.keys(locales)) {
-  const command = `npm run formatjs-compile -- "src/lang/${locale}.json" --ast --out-file "src/lang/compiled/${locale}.json"`;
+  const command = `npm run formatjs-compile -- "src/lang/${locale}.json" --ast --out-file "src/lang/compiled/${locale}.json" --format simple`;
 
   exec(command, (error, stdout, stderr) => {
     if (error) {
